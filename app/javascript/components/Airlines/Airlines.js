eb4c323e3f489b2177ff5523bffea8999e5fe475
Sonu Pandey <sonu.pandey@4moles.com>
2022-01-10 23:24:59 +0530

    React Router added

---
+++ app/javascript/components/Airlines/Airlines.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Airlines = () => {
+	return (<div>Airlines Component to be shown in the box of hte </div>)
+}
+
+export default Airlines
---
+++ app/javascript/components/App.js
@@ -0,0 +1,17 @@
+import React from "react"
+import { Route, Routes } from 'react-router-dom'
+import Airlines from './Airlines/Airlines'
+import Airline from './Airline/Airline'
+
+
+
+const App = () => {
+   return (
+    <Routes>
+      <Route exact path="/" element={<Airlines/>} />
+      <Route exact path="/airlines/:slug" component={Airline} />
+    </Routes>
+    );
+}
+
+export default App;
